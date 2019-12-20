4d0d23aac96ba31e7530782dbd798f84bf4beeb2
Zach <zahngol@gmail.com>
2019-12-19 22:07:10 -0500

    Initial project commit

---
+++ Assets/Scripts/Impulse.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Impulse : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 direction;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = direction * speed;
+    }
+}
---
+++ Assets/Scripts/PaddleMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.zahngol.pong
+{
+    public class PaddleMovement : MonoBehaviour
+    {
+        [SerializeField] private KeyCode upKey;
+        [SerializeField] private KeyCode downKey;
+        [SerializeField] [Range(0,0.5f)] private float speed = 0.25f;
+
+        void Update()
+        {
+            if (Input.GetKey(upKey))
+            {
+                transform.Translate(0,speed,0);
+            }
+            if (Input.GetKey(downKey))
+            {
+                transform.Translate(0,-speed,0);
+            }
+        }
+    }
+}
