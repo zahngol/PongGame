2564cdeaf691f0ab760f30a26be22216da62c8a5
Zach <zahngol@gmail.com>
2019-12-19 22:52:58 -0500

    Initial paddle deflection

---
+++ Assets/Scripts/PaddleDeflection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleDeflection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.name == "Ball")
+        {
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.velocity = rb.velocity * 1.1f;
+        }
+    }
+}
